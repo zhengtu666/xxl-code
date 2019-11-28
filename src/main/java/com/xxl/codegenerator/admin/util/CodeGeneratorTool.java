ee8c2835eb09d1a85455cefe8b4fa0a69d0b753e
liuhuan <liuhuan-cd@duia.com>
2019-11-28 16:02:14 +0800

    项目上传

---
+++ src/main/java/com/xxl/codegenerator/admin/util/CodeGeneratorTool.java
@@ -0,0 +1,25 @@
+package com.xxl.codegenerator.admin.util;
+
+import com.xxl.codegenerator.admin.model.ClassInfo;
+
+
+import java.io.IOException;
+
+/**
+ * code generate tool
+ *
+ * @author xuxueli 2018-04-25 16:29:58
+ */
+public class CodeGeneratorTool {
+
+	/**
+	 * process Table Into ClassInfo
+	 *
+	 * @param tableSql
+	 * @return
+	 */
+	public static ClassInfo processTableIntoClassInfo(String tableSql) throws IOException {
+		return TableParseUtil.processTableIntoClassInfo(tableSql);
+	}
+
+}
